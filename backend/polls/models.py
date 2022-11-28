0a6c865d508fcaecbec8c96454b606f456940558
Sage <salsaeed999@gmail.com>
2022-11-28 14:08:20 -0500

    setup the scheme of the database

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+
+
---
+++ backend/readme.md
@@ -0,0 +1,18 @@
+# Django App (backend)
+
+## To make model changes:
+
+1. Change your models in models.py
+
+
+2. Create make migrations for thoses changes
+```
+python3 manage.py makemigrations
+```
+
+3. Apply thoses changes to the database
+```
+python manage.py migrate 
+```
+
+
