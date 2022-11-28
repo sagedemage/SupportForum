1f543735f1ba8e43b169d128818af9d87a9028ca
Salmaan Saeed <salsaeed999@gmail.com>
2022-11-27 19:15:19 -0500

    the app is able to specify user database schema

--- backend/src/main/java/com/example/support_forum/SupportForumApplication.java
+++ backend/src/main/java/com/example/support_forum/SupportForumApplication.java
@@ -2,11 +2,8 @@ package com.example.support_forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@ComponentScan("com.example.support_forum.db.repositories")
 public class SupportForumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SupportForumApplication.class, args);
--- backend/src/main/java/com/example/support_forum/db/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.support_forum.db.repositories;
-
-import com.example.support_forum.db.models.Users;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Users, Integer> {
-
-}
