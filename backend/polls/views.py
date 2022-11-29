c3f41599101ebbb07ef9692629cbc5ec76060c8b
Sage <salsaeed999@gmail.com>
2022-11-28 22:27:59 -0500

    added a rest api to add a user to the database

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,14 +1,28 @@
-from django.http import HttpResponse
+import json
 
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt;
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from .models import User
 # Create your views here.
 
-from . import models
-
+@api_view(['GET'])
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index")
 
+
+@csrf_exempt
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User(username=serializer.data.get("username"), password=serializer.data.get("password"))
+            user.save()
+            return HttpResponse("User Registration Success")
 def login(request):
-    user = models.User(username="test1000", password="test1000")
-    user.save()
-    return HttpResponse("New User added")
+    return "Hello"
+
 
