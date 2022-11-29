024c5b54301684f665937fe34498502c653d4cce
Sage <salsaeed999@gmail.com>
2022-11-28 22:45:59 -0500

    added rest apis for adding and viewing posts

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('register', views.register, name='register'),
-        path('login', views.login, name='login')
+        path('login', views.login, name='login'),
+        path('add-post', views.add_post, name='add_post'),
+        path('view-posts', views.view_posts, name='view_posts')
         ]
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt;
 from rest_framework.decorators import api_view
-from .serializers import UserSerializer
-from .models import User
+from .serializers import UserSerializer, PostSerializer
+from .models import User, Post
 # Create your views here.
+
 
 @api_view(['GET'])
 def index(request):
@@ -22,6 +23,25 @@ def register(request):
             user = User(username=serializer.data.get("username"), password=serializer.data.get("password"))
             user.save()
             return HttpResponse("User Registration Success")
+
+
+@csrf_exempt
+@api_view(['POST'])
+def add_post(request):
+    if request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            post = Post(title=serializer.data.get("title"), description=serializer.data.get("description"))
+            post.save()
+            return HttpResponse("Added Post")
+
+@csrf_exempt
+@api_view(['GET'])
+def view_posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return JsonResponse({'posts': serializer.data})
+
 def login(request):
     return "Hello"
 
