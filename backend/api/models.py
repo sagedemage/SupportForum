8d1ce80dbf2303791dfd67f488177e84199e8586
Sage <salsaeed999@gmail.com>
2022-12-03 02:04:02 -0500

    changed the models for the user and post tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,14 +4,17 @@ from django.db import models
 
 
 class User(models.Model):
-    email = models.EmailField(max_length=200, null=True)
-    username = models.CharField(max_length=200)
+    email = models.EmailField(null=True)
+    username = models.CharField(max_length=100)
     password = models.CharField(max_length=200)
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.TextField(max_length=15000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
 
 
 
