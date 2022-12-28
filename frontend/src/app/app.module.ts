55a5a576379bbeec613a911977f14d0151c76d38
Salmaan Saeed <salsaeed999@gmail.com>
2022-12-27 22:05:48 -0500

    The navbar menu opens a side nav that contains the navigation links.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,29 @@
+// Angular Libraries
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app_routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// My Components
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AboutComponent } from './components/about/about.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavigationBarComponent } from './components/navigation_bar/navigation_bar.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+// Ng-Boostrap Libraries
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Material UI Component Libraries
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -29,14 +38,15 @@ import { MatListModule } from '@angular/material/list';
   imports: [
     BrowserModule,
     AppRoutingModule,
-	BrowserAnimationsModule,
-	FormsModule,
-	ReactiveFormsModule,
- 	NgbModule,
-	MatToolbarModule,
-	MatIconModule,
-	MatMenuModule,
-	MatListModule,
+	  BrowserAnimationsModule,
+	  FormsModule,
+	  ReactiveFormsModule,
+ 	  NgbModule,
+	  MatToolbarModule,
+	  MatIconModule,
+	  MatMenuModule,
+	  MatListModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
