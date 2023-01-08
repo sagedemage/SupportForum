e9c35887b2bb7dc911527370ce12150067203439
Salmaan Saeed <salsaeed999@gmail.com>
2023-01-07 20:41:02 -0500

    made status into as a varibale for the class

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from "@angular/core";
-//import axios from "axios";
 import Cookies from "universal-cookie";
 
 import { HttpClient } from "@angular/common/http";
 
+
 @Injectable()
 export class AuthService {
     private url = 'http://localhost:8000/api/get-decoded-token';
-    constructor(private http: HttpClient) { }
+    status = false;
+    constructor(private http: HttpClient) {}
     public isAuthenticated(): boolean {
         const cookies = new Cookies();
         const token = cookies.get("token");
-        let status = false;
-
         if (token != undefined) {
             const request = this.http.post(this.url, {"token": token});
             
             request.subscribe({
                 next: (response: any) => {
                     //console.log(response.auth); 
-                    status = response.auth;
-                    console.log(status);
+                    this.status = response.auth;
+                    console.log(this.status);
                 },
                 error: (e) => {
                     console.log(e);
@@ -31,6 +30,6 @@ export class AuthService {
             });
         }
 
-        return status;
+        return this.status;
     }
 }
